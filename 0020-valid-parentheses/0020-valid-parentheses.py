65e192f0bcddcf9ddc05a3d1d6ad80e3735ba35a
geonho <90674760+standpot@users.noreply.github.com>
2026-01-10 15:31:49 +0900

    Time: 0 ms (100%), Space: 19.5 MB (5%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -2,16 +2,16 @@ class Solution:
     def isValid(self, s: str) -> bool:
         temp = ""
         for i in range(0, len(s), 1):
-            if((s[i] == "(") or (s[i] == "[") or (s[i] == "{")):
+            if((s[i] == "(") or (s[i] == "[") or (s[i] == "{")): #open 하는 괄호는 저장
                 temp += s[i]
             else:
                 if(temp == ""):
                     return False
                 else:
-                    if ((temp[-1] == "(" and s[i] == ")") or (temp[-1] == "[" and s[i] == "]") or (temp[-1] == "{" and s[i] == "}")):
-                        temp = temp[:-1]
+                    if ((temp[-1] == "(" and s[i] == ")") or (temp[-1] == "[" and s[i] == "]") or (temp[-1] == "{" and s[i] == "}")): #open 괄호중 가장 최근에 저장 된 것을 비교.
+                        temp = temp[:-1] #일치할 시 가장 최근 저장 된 open 괄호는 삭제, 다음으로 가장 최근에 저장 된 값과 비교
                     else:
-                        return False
+                        return False 
         if temp == "":
             return True
         else:
