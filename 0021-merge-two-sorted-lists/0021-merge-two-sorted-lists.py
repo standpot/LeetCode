cd10647072ec8e81b72e0eb0a3a1272284dc57e7
geonho <90674760+standpot@users.noreply.github.com>
2026-05-10 15:56:56 +0900

    Time: 3 ms (13.7%), Space: 19.4 MB (6.39%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
@@ -6,14 +6,14 @@
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
 
-        if((not(list1)) and (not(list2))):
+        if((not(list1)) and (not(list2))):  # 둘 다 빈 리스트인 경우
             return None
-        elif((not(list1)) or (not(list2))):
-            return list1 or list2
+        elif((not(list1)) or (not(list2))): # 둘 중 하나만 빈 리스트인 경우
+            return list1 or list2           # 값이 있는 리스트만 리턴
         else:
-            head = tail = ListNode()
+            head = tail = ListNode()        # tail, head는 같은 주소를 가리킴
 
-            while(list1 and list2):
+            while(list1 and list2):         # 뛟우순다렙뷈릵
                 if(list1.val < list2.val):
                     tail.next = list1
                     list1 = list1.next
